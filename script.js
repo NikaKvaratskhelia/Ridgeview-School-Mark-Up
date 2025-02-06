a8931280ade436828429cf3bea7c5917b1edf8e5
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2025-02-07 02:20:35 +0400

    parent and students added, header postiion bug fix

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-window.onscroll = function() {
-    removeStickyFunction();
-};
-
+let lastScrollTop = 0;
+const header = document.getElementById("header");
+ 
 function removeStickyFunction() {
-    const header = document.getElementById('header');
-    const scrollThreshold = 300;
-    
-    if (window.innerWidth < 541) {
-        if (window.scrollY > scrollThreshold) {
-            header.classList.remove('visible');
-            header.style.position = 'static';
-        } else {
-            header.classList.add('visible');
-            header.style.position = 'sticky';
-        }
-    }
+  if (!header) return;
+ 
+  let scrollTop = window.scrollY;
+  let isSmallScreen = window.innerWidth < 541;
+ 
+  if (scrollTop > 500 && isSmallScreen && scrollTop > lastScrollTop) {
+    header.style.transform = "translateY(-100%)";
+    header.style.transition = "transform 0.3s ease";
+  }
+ 
+  if (scrollTop < lastScrollTop - 50) {
+    header.style.position = "fixed";
+    header.style.top = "0";
+    header.style.transform = "translateY(0%)";
+  }
+ 
+  lastScrollTop = scrollTop;
 }
+ 
+ 
+window.addEventListener("scroll", removeStickyFunction);
+ 
+ 
 
 // document.addEventListener("DOMContentLoaded", () => {
 //     const animatedElements = document.querySelectorAll(".animated");
