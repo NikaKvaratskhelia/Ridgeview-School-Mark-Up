6328265fc1c089c101282b547cb3890f3f7d6f62
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2025-02-10 16:45:29 +0400

    Events ineer pages added

---
+++ EventPages.js
@@ -0,0 +1,21 @@
+document.getElementById('button-rsvp').addEventListener('click', function() {
+    document.getElementById('form-div').style.display = 'flex';
+    document.getElementById('main').style.display = 'none';
+  });
+  
+  document.getElementById('button-rsvp1').addEventListener('click', function() {
+    document.getElementById('form-div').style.display = 'flex';
+    document.getElementById('main').style.display = 'none';
+  });
+
+  document.getElementById('submit').addEventListener('click', function() {
+    document.getElementById('registered-div').style.display = 'flex';
+    document.getElementById('main').style.display = 'none';
+    document.getElementById('form-div').style.display = 'none'
+  });
+
+
+
+  
+
+
